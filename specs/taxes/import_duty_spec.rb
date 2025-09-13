867f3ddbe47d5dfd8578a8fbbff8193f40b6d1b6
troccoli <pedroaugustobelzkai@gmail.com>
2025-09-13 12:00:49 -0300

    test: add RSpec tests for ImportDutyTax to validate tax rate, product matching, and tax application

---
+++ specs/taxes/import_duty_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../../taxes/import_duty'
+
+RSpec.describe ImportDutyTax do
+  let (:import_duty_tax) { ImportDutyTax.new }
+
+  describe 'Constants' do
+    it 'should have a contanst rate of 5%' do
+      expect(ImportDutyTax::RATE).to eq(BigDecimal('0.05'))
+    end
+  end
+
+  describe '#match?' do
+    it 'should return true if the product is imported' do
+      product = Product.new('Product', 'book', 10.0, true)
+      expect(import_duty_tax.match?(product)).to be_truthy
+    end
+
+    it 'should return false if the product is not imported' do
+      product = Product.new('Product', 'technology', 10.0, false)
+      expect(import_duty_tax.match?(product)).to be_falsey
+    end
+  end
+
+  describe "#apply_tax" do
+    it 'should return 0 if the product is not imported' do
+      product = Product.new('Product', 'book', 10.0, false)
+      expect(import_duty_tax.apply_tax(product)).to eq(BigDecimal('0'))
+    end
+
+    it 'should return the tax amount for a product that is imported' do
+      product = Product.new('Product', 'technology', 10.0, true)
+      expect(import_duty_tax.apply_tax(product)).to eq(BigDecimal('0.5'))
+    end
+  end
+end
