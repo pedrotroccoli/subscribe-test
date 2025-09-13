26ff00f3f2d7b88a39dcf03eccc2ec51957498c6
troccoli <pedroaugustobelzkai@gmail.com>
2025-09-13 11:54:52 -0300

    test: add round_up method tests for TaxBase to validate tax rounding functionality

--- domain/tax_base_spec.rb
+++ domain/tax_base_spec.rb
@@ -13,4 +13,20 @@ RSpec.describe TaxBase do
       expect { tax_base.match?(product) }.to raise_error(NotImplementedError)
     end
   end
+
+  describe '#round_up' do
+    it 'should round up the tax to the nearest 0.05' do
+      list_of_numbers = [
+        0.622,
+        0.623,
+        0.624,
+        0.627,
+      ]
+
+      list_of_numbers.each do |number|
+        correct_number = BigDecimal((number * 20).ceil) / 20.0
+        expect(tax_base.round_up(BigDecimal(number.to_s))).to eq(correct_number)
+      end
+    end
+  end
 end
