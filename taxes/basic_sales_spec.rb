d5c63da10f941f5e3866c9901ab5b9fe5f1104d2
troccoli <pedroaugustobelzkai@gmail.com>
2025-09-13 11:46:58 -0300

    test: add match? method tests for BasicSalesTax to validate exempt categories

--- taxes/basic_sales_spec.rb
+++ taxes/basic_sales_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe BasicSalesTax do
       expect(BasicSalesTax::RATE).to eq(BigDecimal('0.1'))
     end
   end
+
+  describe '#match?' do
+    it 'should return true if the product is in the EXEMPT_CATEGORIES' do
+      product = Product.new('Product', 'book', 10.0, false)
+      expect(BasicSalesTax.new.match?(product)).to be_truthy
+    end
+
+    it 'should return false if the product is not in the EXEMPT_CATEGORIES' do
+      product = Product.new('Product', 'technology', 10.0, false)
+      expect(BasicSalesTax.new.match?(product)).to be_falsey
+    end
+  end
 end
