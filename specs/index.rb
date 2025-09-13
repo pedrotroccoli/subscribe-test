854a2facf740f10358d28ab6b0b81d86e4be9ff4
troccoli <pedroaugustobelzkai@gmail.com>
2025-09-13 12:25:59 -0300

    refactor: update index.rb to require specs from the 'implementations' folder

--- specs/index.rb
+++ specs/index.rb
@@ -1,5 +1,5 @@
 require_relative './domain/tax_base_spec'
 require_relative './domain/tax_manager_spec'
 
-require_relative './taxes/basic_sales_spec'
-require_relative './taxes/import_duty_spec'
+require_relative './implementations/basic_sales_spec'
+require_relative './implementations/import_duty_spec'
