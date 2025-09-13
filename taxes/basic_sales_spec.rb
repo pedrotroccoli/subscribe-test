d41064e2710a5e3a16ab427978640b354b4d6da1
troccoli <pedroaugustobelzkai@gmail.com>
2025-09-13 11:43:54 -0300

    test: add RSpec tests for BasicSalesTax including exempt categories and tax rate

---
+++ taxes/basic_sales_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'basic_sales'
+
+RSpec.describe BasicSalesTax do
+  describe 'EXEMPT_CATEGORIES' do
+    it 'should return a list of prefixed categories' do
+      expect(BasicSalesTax::EXEMPT_CATEGORIES).to eq(['book', 'food', 'medical'])
+    end
+
+    it 'should have a contanst rate of 10%' do
+      expect(BasicSalesTax::RATE).to eq(BigDecimal('0.1'))
+    end
+  end
+end
