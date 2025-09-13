5fc5b102be5b43973cf32bd7e2cc0adf8e328506
troccoli <pedroaugustobelzkai@gmail.com>
2025-09-13 10:49:09 -0300

    feat: update technology product price to 15.32 in `init.rb` for accurate tax calculation

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ puts "Hello Subscribe"
 BasicSalesTaxClass = BasicSalesTax.new
 
 without_tax = Struct.new(:category, :price).new('book', 10.00)
-with_tax = Struct.new(:category, :price).new('technology', 15.00)
+with_tax = Struct.new(:category, :price).new('technology', 15.32)
 
 puts BasicSalesTaxClass.apply_tax(without_tax)
 puts BasicSalesTaxClass.apply_tax(with_tax)
