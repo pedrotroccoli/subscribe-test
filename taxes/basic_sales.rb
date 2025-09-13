9473167de508c174fd4ed3e2252f33d804f0d713
troccoli <pedroaugustobelzkai@gmail.com>
2025-09-13 10:51:36 -0300

    feat: refactor tax calculation in `BasicSalesTax` to use a constant rate and improve readability

--- taxes/basic_sales.rb
+++ taxes/basic_sales.rb
@@ -1,14 +1,20 @@
 require_relative '../domain/tax_base'
 
 class BasicSalesTax < TaxBase
+  RATE = BigDecimal('0.1')
+
   def apply_tax(product)
     if product.category.to_s == 'book' || product.category.to_s == 'food' || product.category.to_s == 'medical'
       return BigDecimal('0')
     end
 
-    tax = product.price * 0.1
+    tax = product.price * RATE
 
-    # Round up to the nearest 0.05
+    round_up(tax)
+  end
+
+  # Round up to the nearest 0.05
+  def round_up(tax)
     (tax * 20).ceil / 20.0
   end
 end
