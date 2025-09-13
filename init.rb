ec58801e25cec20245923408988bdb6a71f9b44c
troccoli <pedroaugustobelzkai@gmail.com>
2025-09-13 10:47:10 -0300

    feat: add initial implementation of sales tax application in `init.rb`

--- init.rb
+++ init.rb
@@ -1,1 +1,15 @@
+require 'bigdecimal'
+
+require_relative 'taxes/basic_sales'
+
 puts "Hello Subscribe"
+
+# First test
+
+BasicSalesTaxClass = BasicSalesTax.new
+
+without_tax = Struct.new(:category, :price).new('book', 10.00)
+with_tax = Struct.new(:category, :price).new('technology', 15.00)
+
+puts BasicSalesTaxClass.apply_tax(without_tax)
+puts BasicSalesTaxClass.apply_tax(with_tax)
