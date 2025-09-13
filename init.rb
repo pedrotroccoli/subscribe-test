0807dcd761d2d2dd4d4e0d7452090d6aa4e2ec81
troccoli <pedroaugustobelzkai@gmail.com>
2025-09-13 10:59:45 -0300

    feat: integrate ImportDutyTax calculations into `init.rb` and update tax application logic

--- init.rb
+++ init.rb
@@ -1,15 +1,22 @@
 require 'bigdecimal'
 
 require_relative 'taxes/basic_sales'
+require_relative 'taxes/import_duty'
 
 puts "Hello Subscribe"
 
 # First test
 
 BasicSalesTaxClass = BasicSalesTax.new
+ImportDutyTaxClass = ImportDutyTax.new
 
-without_tax = Struct.new(:category, :price).new('book', 10.00)
-with_tax = Struct.new(:category, :price).new('technology', 15.32)
+without_basic_sales_tax = Struct.new(:category, :price, :imported).new('book', 10.00, false)
+with_basic_sales_tax = Struct.new(:category, :price, :imported).new('technology', 15.32, false)
 
-puts BasicSalesTaxClass.apply_tax(without_tax)
-puts BasicSalesTaxClass.apply_tax(with_tax)
+without_import_duty_tax = Struct.new(:category, :price, :imported).new('book', 10.00, false)
+with_import_duty_tax = Struct.new(:category, :price, :imported).new('book', 15.32, true)
+
+puts BasicSalesTaxClass.apply_tax(without_basic_sales_tax)
+puts BasicSalesTaxClass.apply_tax(with_basic_sales_tax)
+puts ImportDutyTaxClass.apply_tax(without_import_duty_tax)
+puts ImportDutyTaxClass.apply_tax(with_import_duty_tax)
