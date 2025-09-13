68082faf6e1e6b4c5cbadc9bc28cec83f1548b5a
troccoli <pedroaugustobelzkai@gmail.com>
2025-09-13 11:53:10 -0300

    chore: update spec_helper to include TaxBase

--- spec_helper.rb
+++ spec_helper.rb
@@ -1,4 +1,6 @@
 require 'bigdecimal'
 require_relative './domain/product'
 require_relative './domain/checkout_item'
+require_relative './domain/tax_base_spec'
+
 require_relative './taxes/basic_sales_spec'
