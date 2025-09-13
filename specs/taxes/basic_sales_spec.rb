e69b86529f2d07d6271d950af0c2ee0d6d882962
troccoli <pedroaugustobelzkai@gmail.com>
2025-09-13 12:09:15 -0300

    test: refactor BasicSalesTax specs to use let variables for product instances

--- specs/taxes/basic_sales_spec.rb
+++ specs/taxes/basic_sales_spec.rb
@@ -1,6 +1,10 @@
 require_relative '../../taxes/basic_sales'
 
 RSpec.describe BasicSalesTax do
+  let (:basic_sales_tax) { BasicSalesTax.new }
+  let (:book_product) { Product.new('Product', 'book', 10.0, false) }
+  let (:technology_product) { Product.new('Product', 'technology', 10.0, false) }
+
   describe 'Constants' do
     it 'should return a list of prefixed categories' do
       expect(BasicSalesTax::EXEMPT_CATEGORIES).to eq(['book', 'food', 'medical'])
@@ -13,25 +17,21 @@ RSpec.describe BasicSalesTax do
 
   describe '#match?' do
     it 'should return true if the product is in the EXEMPT_CATEGORIES' do
-      product = Product.new('Product', 'book', 10.0, false)
-      expect(BasicSalesTax.new.match?(product)).to be_truthy
+      expect(basic_sales_tax.match?(book_product)).to be_truthy
     end
 
     it 'should return false if the product is not in the EXEMPT_CATEGORIES' do
-      product = Product.new('Product', 'technology', 10.0, false)
-      expect(BasicSalesTax.new.match?(product)).to be_falsey
+      expect(basic_sales_tax.match?(technology_product)).to be_falsey
     end
   end
 
   describe "#apply_tax" do
     it 'should return 0 if the product is in the EXEMPT_CATEGORIES' do
-      product = Product.new('Product', 'book', 10.0, false)
-      expect(BasicSalesTax.new.apply_tax(product)).to eq(BigDecimal('0'))
+      expect(basic_sales_tax.apply_tax(book_product)).to eq(BigDecimal('0'))
     end
 
     it 'should return the tax amount for a product that is not in the EXEMPT_CATEGORIES' do
-      product = Product.new('Product', 'technology', 10.0, false)
-      expect(BasicSalesTax.new.apply_tax(product)).to eq(BigDecimal('1.0'))
+      expect(basic_sales_tax.apply_tax(technology_product)).to eq(BigDecimal('1.0'))
     end
   end
 end
