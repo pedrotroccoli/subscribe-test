2ef726e4be5cbfdd173b6e05dd214f0f3fd61dc2
troccoli <pedroaugustobelzkai@gmail.com>
2025-09-13 11:31:29 -0300

    feat: update product entries in init.rb to reflect new test cases for Checkout

--- init.rb
+++ init.rb
@@ -14,9 +14,13 @@ tax_manager = TaxManager.new([BasicSalesTax.new, ImportDutyTax.new])
 
 checkout = Checkout.new(tax_manager)
 
-checkout.add_product(CheckoutItem.new(Product.new('Tranquility of Mind', 'book', 10.00, false), 1))
-checkout.add_product(CheckoutItem.new(Product.new('Headphone', 'technology', 15.32, false), 1))
-checkout.add_product(CheckoutItem.new(Product.new('The Joy of Cooking', 'book', 10.00, true), 3))
-checkout.add_product(CheckoutItem.new(Product.new('Chocolate Bar', 'food', 15.32, true), 1))
+# Input 01
+# 2 book at 12.49
+# 1 music CD at 14.99
+# 1 chocolate bar at 0.85
+
+checkout.add_product(CheckoutItem.new(Product.new('Tranquility of Mind', 'book', 12.49, false), 2))
+checkout.add_product(CheckoutItem.new(Product.new('Music CD', 'technology', 14.99, false), 1))
+checkout.add_product(CheckoutItem.new(Product.new('Chocolate Bar', 'food', 0.85, false), 1))
 
 checkout.generate_receipt
