5140cde35d31945a52eaae6cfcb1ed04d8fe2c8e
troccoli <pedroaugustobelzkai@gmail.com>
2025-09-13 11:09:27 -0300

    feat: update receipt generation to display item details

--- checkout.rb
+++ checkout.rb
@@ -9,6 +9,10 @@ class Checkout
 
   def generate_receipt
     puts "All products: \n\n"
+    @items.each do |item|
+      puts "#{item.quantity} #{item.product.name} #{item.product.price}"
+    end
+    puts "\n"
     puts "Sales Taxes: $0,00"
     puts "Total: $0,00 \n\n"
   end
--- init.rb
+++ init.rb
@@ -4,12 +4,12 @@ require_relative 'checkout'
 
 # Second test
 
-Product = Struct.new(:category, :price, :imported)
+Product = Struct.new(:name, :category, :price, :imported)
 CheckoutItem = Struct.new(:product, :quantity)
 
 checkout = Checkout.new
-checkout.add_product(CheckoutItem.new(Product.new('book', 10.00, false), 1))
-checkout.add_product(CheckoutItem.new(Product.new('technology', 15.32, false), 1))
-checkout.add_product(CheckoutItem.new(Product.new('book', 10.00, true), 1))
-checkout.add_product(CheckoutItem.new(Product.new('book', 15.32, true), 1))
+checkout.add_product(CheckoutItem.new(Product.new('Tranquility of Mind', 'book', 10.00, false), 1))
+checkout.add_product(CheckoutItem.new(Product.new('Headphone', 'technology', 15.32, false), 1))
+checkout.add_product(CheckoutItem.new(Product.new('The Joy of Cooking', 'book', 10.00, true), 1))
+checkout.add_product(CheckoutItem.new(Product.new('Chocolate Bar', 'food', 15.32, true), 1))
 checkout.generate_receipt
