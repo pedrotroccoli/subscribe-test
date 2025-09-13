cb5f9981a074c91ed2f0027e62875b04afcb3746
troccoli <pedroaugustobelzkai@gmail.com>
2025-09-13 11:18:29 -0300

    feat: integrate tax calculations into receipt generation for Checkout class

--- checkout.rb
+++ checkout.rb
@@ -1,3 +1,6 @@
+require_relative 'taxes/basic_sales'
+require_relative 'taxes/import_duty'
+
 class Checkout
   def initialize
     @items = []
@@ -10,15 +13,22 @@ class Checkout
   def generate_receipt
     puts "All products: \n\n"
     total = 0
+    taxes = 0
 
     @items.each do |item|
-      total += item.product.price * item.quantity
+      basic_tax = BasicSalesTax.new.apply_tax(item.product)
+      import_duty = ImportDutyTax.new.apply_tax(item.product)
 
-      puts "#{item.quantity} #{item.product.name} #{item.product.price}"
+      all_taxes = basic_tax + import_duty
+
+      total += (item.product.price + all_taxes) * item.quantity
+      taxes += all_taxes * item.quantity
+
+      puts "#{item.quantity} #{item.product.name} #{item.product.price} - #{basic_tax.to_f.round(2)} + #{import_duty.to_f.round(2)} - #{(all_taxes * item.quantity).to_f.round(2)}"
     end
 
     puts "\n"
-    puts "Sales Taxes: $0,00"
-    puts "Total: $ #{total} \n\n"
+    puts "Sales Taxes: $ #{taxes.to_f.round(2)}"
+    puts "Total: $ #{total.to_f.round(2)} \n\n"
   end
 end
